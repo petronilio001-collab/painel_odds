4c3ac30e2acd0c529e7a771dad0d041a2ca96798
petronilio001-collab <petronilio.001@gmail.com>
2025-08-26 15:18:44 -0300

    Update monitor_odds.py

--- monitor_odds.py
+++ monitor_odds.py
@@ -1,10 +1,15 @@
-import csv
-import time
-from datetime import datetime
-from selenium import webdriver
+
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
+
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.binary_location = "/usr/bin/chromium-browser"
+
+service = Service("/usr/bin/chromedriver")
+driver = webdriver.Chrome(service=service, options=chrome_options)
+
 
 # Configurações do navegador
 chrome_path = "./chromedriver"
